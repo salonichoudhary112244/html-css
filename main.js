4dcdfdc35c71471981e3568ee0028e4e0a7f3717
salonichoudhary <salonichoudhary112244@gmail.com>
2025-03-24 05:54:03 -0700

    js

---
+++ main.js
@@ -0,0 +1,19 @@
+// import add from '/fun1.js'
+// let ans=add(2,5)
+// console.log(ans);
+
+// import{add,sub} from'./fun1.js'
+
+// let ans=add(12,5)
+// console.log(ans);
+
+// let sn1=sub(12,5)
+// console.log(sn1)
+
+import{add as Sum,sub as Minus} from'./fun1.js'
+
+let ans=Sum(12,5)
+console.log(ans);
+
+let sn1=Minus(12,5)
+console.log(sn1)
