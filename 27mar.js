15875e55129ded931bdc35bc2d287d0796c991a6
salonichoudhary <salonichoudhary112244@gmail.com>
2025-03-26 10:14:14 -0700

    js

---
+++ 27mar.js
@@ -0,0 +1,17 @@
+let set="welcome"
+console.log(set);
+
+
+let name="saloni"
+let name1=`my name is ${name}`
+console.log(name1);
+
+let age="saloni"
+console.log(age.toUpperCase());
+
+
+let address="BHOPAL"
+console.log(address.toLowerCase());
+
+let r=address.repeat(10)
+console.log(r);
